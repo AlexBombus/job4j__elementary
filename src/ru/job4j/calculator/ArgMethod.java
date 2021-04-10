b85902fdf7b95f9d6fb052a6b50fd1dd11182f3a
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-04-10 12:20:00 +0300

    Конвертер валюты [#516147]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -5,12 +5,17 @@ public class ArgMethod {
     public static void hello(String name) {
         System.out.println("Hello, " + name);
     }
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", " + age);
+    }
+
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
 
         ArgMethod.hello(name);
+        ArgMethod.hello(name, age);
 
     }
 }
