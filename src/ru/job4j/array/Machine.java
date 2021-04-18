6273b63ca194d7bb7e220c069cacd465fe6ea7bd
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-04-18 13:30:19 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#516116]

--- src/ru/job4j/array/Machine.java
+++ src/ru/job4j/array/Machine.java
@@ -4,20 +4,17 @@ import java.util.Arrays;
 
 public class Machine {
     public static int[] change(int money, int price) {
-        int[] coins = {10, 5, 2, 1};
-        int[] rsl = new int[100]; // это массив, содержащий монеты, которыми дается сдача
+        int[] coins = {10, 5, 2, 1}; // массив с типом имеющихся монет
+        int[] rsl = new int[100]; // это массив, с перечнем монет сдачи
         int size = 0; // размер массива сдачи - колличество монет сдачи
-        int moneyBack = money - price;
-        while (moneyBack > 0) {
-            for (int i = 0; i < coins.length; i++) {
-                if (moneyBack >= coins[i]) {
-                    moneyBack -= coins[i];
-                    rsl[size] = coins[i];
-                    size++;
-                    break;
-                }
-            }
-        }
-        return Arrays.copyOf(rsl, size);
+        int moneyBack = money - price; // расчет сдачи
+        for (int i = 0; i < coins.length; i++) {
+            while (moneyBack >= coins[i]) {
+                moneyBack -= coins[i];
+                rsl[size] = coins[i];
+                size++;
+                }       // метод Arrays.copyOf() сокращает размер массива с изначального rls
+            }           //  до размера кол-ва полученых монет - size
+        return Arrays.copyOf(rsl, size); // метод  change возвращает массив rls, содержащий перечень монет сдачи.
     }
 }
