0bc3a517a99e92f1c2f16149000d92ab797b9885
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-04-18 23:44:48 +0300

    + Javadoc

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -1,6 +1,10 @@
 package ru.job4j.array;
 
 public class MatrixCheck {
+    /***
+     *  метод проверяет что строка  row в двухмерном массиве целиком заполнена символом 'X'.
+     * @param row - индекс проверяемой строки - char[row][] board;
+     */
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board[row].length; i++) {
@@ -9,28 +13,38 @@ public class MatrixCheck {
                 break;
             }
         }
-        return result; // метод проверяет что строка  row в двухмерном массиве целиком заполнена символом 'X'.
+        return result;
     }
 
+    /***
+     *  метод проверяет что в каждой строке двухмерного массива  ячейка с индексом column заполнена символом 'X'.
+     * @param column  индекс ячейки в строке - char[][row] board;
+     */
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][column] != 'X') {
+        for (char[] chars : board) {
+            if (chars[column] != 'X') {
                 result = false;
                 break;
             }
-        }               // метод проверяет что в каждой строке двухмерного массива
-        return result; // ячейка с индексом column заполнена символом 'X'.
+        }
+        return result;
     }
 
+    /***
+     * метод заполняет одномерный массив элементами диагонали из двухмерного массива
+     */
     public static char[] extractDiagonal(char[][] board) {
         char[] rsl = new char[board.length];
         for (int i = 0; i < board.length; i++) {
             rsl[i] = board[i][i];
         }
-        return rsl; //метод заполняет одномерный массив элементами диагонали из двухмерного массива
+        return rsl;
     }
 
+    /**
+     * Метод проверяет наличие горизонт. или верт. строки полностью заполненной 'Х'
+     */
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
@@ -39,6 +53,7 @@ public class MatrixCheck {
                 break;
             }
         }
-        return result; // метод проверяет наличие горизонт. или верт. строки полностью заполненной 'Х'
+        return result;
     }
 }
+
