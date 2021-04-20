c4fcab865297fca7fb24eab9ab1c07f1d8f9a20c
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-04-20 23:04:59 +0300

    Coment

--- src/ru/job4j/array/Square.java
+++ src/ru/job4j/array/Square.java
@@ -1,6 +1,12 @@
 package ru.job4j.array;
 
 public class Square {
+    /**
+     * Метод заполняет массив заданной длины значениями индекса в квадрате
+     * например array[2] = 4; (2*2)   array[3] = 9; (3*3)
+     * @param bound - длина массива
+     * @return массив заполненный значениями
+     */
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
         for (int i = 0; i < rst.length; i++) {
