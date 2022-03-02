e603b22edbab7bc6280946b549f38162e86b5b7a
a.elizarov <vdv254817>
2022-03-02 10:49:32 +0300

    1.9. Статические и не статические методы [#504806 #270691] + замена однострочных комментариев на JavaDoc

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -2,18 +2,24 @@ package ru.job4j.array;
 
 public class SwitchArray {
 
+    /**
+     * Метод меняет местами  значения  указанных индексов массива
+     */
     public static int[] swap(int[] array, int source, int dest) {
         int temp = array[source];
         array[source] = array[dest];
         array[dest] = temp;
-        return array; // меняет местами  значения  указанных индексов массива
+        return array;
     }
 
+    /**
+     * Метод меняет местами первый и последний элементы в массиве
+     */
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
         array[array.length - 1] = temp;
-        return array; // меняет местами нулевой элемент и последний элемент в массиве
+        return array;
     }
 
     public static void main(String[] args) {
